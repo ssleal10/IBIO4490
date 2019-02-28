9ff115f90da9065df1c0197f70803fb0b382890a
ssleal10 <47038702+ssleal10@users.noreply.github.com>
2019-02-28 16:58:23 -0500

    Update train.py

--- 05-Textons/python/train.py
+++ 05-Textons/python/train.py
@@ -301,7 +301,7 @@ while num_a <= limite_a:
     from sklearn.neighbors import KNeighborsClassifier
     from sklearn.ensemble import RandomForestClassifier
     
-    modelo_KNN = KNeighborsClassifier(n_neighbors=200,p = 2)
+    modelo_KNN = KNeighborsClassifier(n_neighbors=5,p = 2)
     modelo_KNN.fit(histogramas, LabelsBalanced)
     prediction_KNN = modelo_KNN.predict(histogramas)
     
