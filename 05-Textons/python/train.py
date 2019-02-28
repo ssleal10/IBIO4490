6c1dc89e37b697987f65d59ffe70abe64df9c56a
ssleal10 <47038702+ssleal10@users.noreply.github.com>
2019-02-28 17:58:23 -0500

    try

--- 05-Textons/python/train.py
+++ 05-Textons/python/train.py
@@ -305,7 +305,7 @@ while num_a <= limite_a:
     
     histogramas = SelectKBest(chi2, k=2).fit_transform(histogramas, LabelsBalanced)
     
-    modelo_KNN = KNeighborsClassifier(n_neighbors=200)
+    modelo_KNN = KNeighborsClassifier(n_neighbors=100,weights = 'distance')
     #modelo_KNN.fit(hist_new, LabelsBalanced)
     modelo_KNN.fit(histogramas, LabelsBalanced)
     prediction_KNN = modelo_KNN.predict(histogramas)
