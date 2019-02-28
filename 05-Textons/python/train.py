fa70df65d65622ac514fc3adac6caf8ad0d04112
ssleal10 <47038702+ssleal10@users.noreply.github.com>
2019-02-28 09:18:21 -0500

    umcomment matrix

--- 05-Textons/python/train.py
+++ 05-Textons/python/train.py
@@ -316,55 +316,55 @@ while num_a <= limite_a:
     
     class_names = np.arange(0,10)
     
-#    import itertools
+    import itertools
     from sklearn.metrics import confusion_matrix
-#    def plot_confusion_matrix(cm, classes,
-#                              normalize=False,
-#                              title='Confusion matrix',
-#                              cmap=plt.cm.Blues):
+    def plot_confusion_matrix(cm, classes,
+                              normalize=False,
+                              title='Confusion matrix',
+                              cmap=plt.cm.Blues):
 #        """
 #        This function prints and plots the confusion matrix.
 #        Normalization can be applied by setting `normalize=True`.
 #        """
-#        if normalize:
-#            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-#            print("Normalized confusion matrix")
-#        else:
-#            print('Confusion matrix, without normalization')
+        if normalize:
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+            print("Normalized confusion matrix")
+        else:
+            print('Confusion matrix, without normalization')
 #    
-#        print(cm)
+        print(cm)
 #    
-#        plt.imshow(cm, interpolation='nearest', cmap=cmap)
-#        plt.title(title)
-#        plt.colorbar()
-#        tick_marks = np.arange(len(classes))
-#        plt.xticks(tick_marks, classes, rotation=45)
-#        plt.yticks(tick_marks, classes)
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
 #    
-#        fmt = '.2f' if normalize else 'd'
-#        thresh = cm.max() / 2.
-#        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-#            plt.text(j, i, format(cm[i, j], fmt),
-#                     horizontalalignment="center",
-#                     color="white" if cm[i, j] > thresh else "black")
+        fmt = '.2f' if normalize else 'd'
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, format(cm[i, j], fmt),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
 #    
-#        plt.ylabel('True label')
-#        plt.xlabel('Predicted label')
-#        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.tight_layout()
         
     confusionmat_KNN = confusion_matrix(LabelsBalanced,prediction_KNN)
     confusionmat_TREE = confusion_matrix(LabelsBalanced,prediction_TREE)
-#    np.set_printoptions(precision=2)
+    np.set_printoptions(precision=2)
 #    
-#    # Plot non-normalized confusion matrix
-#    plt.figure()
-#    plot_confusion_matrix(confusionmat_KNN, class_names, normalize =True,
-#                          title='Normalized confusion matrix - KNN')
+    # Plot non-normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(confusionmat_KNN, class_names, normalize =True,
+                          title='Normalized confusion matrix - KNN')
 #    
-#    # Plot normalized confusion matrix
-#    plt.figure()
-#    plot_confusion_matrix(confusionmat_TREE, class_names, normalize=True,
-#                          title='Normalized confusion matrix - RandomForest')
+    # Plot normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(confusionmat_TREE, class_names, normalize=True,
+                          title='Normalized confusion matrix - RandomForest')
 #    
     plt.show()
     print('Con un número de imágenes de:'+str(num_a))
@@ -373,11 +373,11 @@ while num_a <= limite_a:
     
     
     from sklearn.externals import joblib
-    filename = 'model_KNN_FINAL.pk1'
+    filename = 'model_KNN_FINAL2500.pk1'
     joblib.dump(modelo_KNN, filename)
     
     from sklearn.externals import joblib
-    filename = 'model_RandomForest_FINAL.pk1'
+    filename = 'model_RandomForest_FINAL2500.pk1'
     joblib.dump(modelo_TREE, filename)
     
     #ACA_KNN.append(ACA_KNN)
