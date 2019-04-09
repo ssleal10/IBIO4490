77b6abbf40713e378af962c5d4df0a0436295455
ssleal10 <47038702+ssleal10@users.noreply.github.com>
2019-04-09 16:25:43 -0500

    Add files via upload

--- 11-NN/convEmotions.py
+++ 11-NN/convEmotions.py
@@ -21,8 +21,8 @@ class Net(nn.Module):
         #layer with 64 2d convolutional filter of size 3x3
         self.conv1 = nn.Conv2d(1, 64, kernel_size=3) #Channels input: 1, c output: 48, filter of size 3
         self.conv2 = nn.Conv2d(64, 32, kernel_size=3)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=3)
-        self.fc1 = nn.Linear(32, 16)   
+        self.conv3 = nn.Conv2d(32, 16, kernel_size=3)
+        self.fc1 = nn.Linear(16, 16)   
         self.fc2 = nn.Linear(16, 10)  
     
     def forward(self, x, verbose=False):
@@ -39,7 +39,7 @@ class Net(nn.Module):
         x = F.dropout(x, 0.25, training=self.training)
         if verbose: print(x.size())
         #ipdb.set_trace()
-        x = x.view(-1, 32)
+        x = x.view(-1, 16)
         if verbose: print(x.size())
         x = F.relu(self.fc1(x))
         if verbose: print(x.size())
