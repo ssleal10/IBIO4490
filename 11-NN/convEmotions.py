8fa06cf45d4c5dc59cc685987b40d0337b3c3255
ssleal10 <47038702+ssleal10@users.noreply.github.com>
2019-04-09 15:04:24 -0500

    Add files via upload

---
+++ 11-NN/convEmotions.py
@@ -0,0 +1,166 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+#from torchvision import datasets, transforms
+import numpy as np
+import tqdm
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print('device:',device)
+def print_network(model, name):
+    num_params=0
+    for p in model.parameters():
+        num_params+=p.numel()
+    print(name)
+    print(model)
+    print("The number of parameters {}".format(num_params))
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        #layer with 64 2d convolutional filter of size 3x3
+        self.conv1 = nn.Conv2d(1, 48, kernel_size=3) #Channels input: 1, c output: 48, filter of size 3
+        self.conv2 = nn.Conv2d(48, 24, kernel_size=3)
+        self.conv3 = nn.Conv2d(24, 24, kernel_size=3)
+        self.fc1 = nn.Linear(24, 24)   
+        self.fc2 = nn.Linear(24, 10)  
+    
+    def forward(self, x, verbose=False):
+        if verbose: "Output Layer by layer"
+        if verbose: print(x.size())
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2) #Perform a Maximum pooling operation over the nonlinear responses of the convolutional layer
+        if verbose: print(x.size())
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        if verbose: print(x.size())
+        x = F.dropout(x, 0.25, training=self.training)#Try to control overfit on the network, by randomly excluding 25% of neurons on the last #layer during each iteration
+        if verbose: print(x.size())
+        x = F.max_pool2d(F.relu(self.conv3(x)), 2)
+        if verbose: print(x.size())
+        x = F.dropout(x, 0.25, training=self.training)
+        if verbose: print(x.size())
+        #ipdb.set_trace()
+        x = x.view(-1, 24)
+        if verbose: print(x.size())
+        x = F.relu(self.fc1(x))
+        if verbose: print(x.size())
+        x = self.fc2(x)
+        if verbose: print(x.size())
+        return x
+
+    def training_params(self):
+        self.optimizer = torch.optim.SGD(self.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0)
+        self.Loss = nn.CrossEntropyLoss()
+        
+#def get_data(batch_size):
+    #transform_train = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+def get_data():
+    # angry, disgust, fear, happy, sad, surprise, neutral
+    with open("fer2013.csv") as f:
+        content = f.readlines()
+
+    lines = np.array(content)
+    num_of_instances = lines.size
+    print("number of instances: ",num_of_instances)
+    print("instance length: ",len(lines[1].split(",")[1].split(" ")))
+
+    x_train, y_train, x_test, y_test = [], [], [], []
+
+    for i in range(1,num_of_instances):
+        emotion, img, usage = lines[i].split(",")
+        pixels = np.array(img.split(" "), 'float32')
+        emotion = 1 if int(emotion)==3 else 0 # Only for happiness
+        if 'Training' in usage:
+            y_train.append(emotion)
+            x_train.append(pixels)
+        elif 'PublicTest' in usage:
+            y_test.append(emotion)
+            x_test.append(pixels)
+
+    #------------------------------
+    #data transformation for train and test sets
+    x_train = np.array(x_train, 'float64')
+    y_train = np.array(y_train, 'float64')
+    x_test = np.array(x_test, 'float64')
+    y_test = np.array(y_test, 'float64')
+
+    x_train /= 255 #normalize inputs between [0, 1]
+    x_test /= 255
+
+    x_train = x_train.reshape(x_train.shape[0], 48, 48)
+    x_test = x_test.reshape(x_test.shape[0], 48, 48)
+    y_train = y_train.reshape(y_train.shape[0], 1)
+    y_test = y_test.reshape(y_test.shape[0], 1)
+
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
+    
+    return x_train, y_train, x_test, y_test
+
+def get_true_test_data():
+    import cv2
+    import os
+    images = np.zeros((1610,48,48))
+    from tqdm import tqdm
+    for i in tqdm(range(1610), desc = "Detecting,cropping and resizing(48,48) test faces,wait..."):
+        filename = os.listdir('Emotions_test')[i]
+        img = cv2.imread(os.path.join('Emotions_test',filename))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        faces = face_cascade.detectMultiScale(img,1.1,5,0)
+        crop = img[faces[0][1]:faces[0][1]+faces[0][3],faces[0][0]:faces[0][0]+faces[0][2]]
+        img = cv2.resize(crop, dsize=(48, 48), interpolation=cv2.INTER_CUBIC)
+        images[i,:,:]= img
+    return images
+
+def train(x_train, y_train, model):
+    for epoch in range(epochs):
+        model.train()
+        loss_cum = []
+        Acc = 0
+        for batch_idx in tqdm.tqdm(range(0, len(x_train), batch_size), desc="[TRAIN] Epoch: {}".format(epoch)):
+            x_batch = x_train[batch_idx:batch_idx+batch_size]
+            y_batch = y_train[batch_idx:batch_idx+batch_size]
+            data = torch.from_numpy(x_batch).float().to(device)
+            target = torch.from_numpy(y_batch).float().to(device)
+            
+            output = model(data)
+            model.optimizer.zero_grad()
+            loss = model.Loss(output,target)   
+            loss.backward()
+            model.optimizer.step()
+            loss_cum.append(loss.item())
+            _, arg_max_out = torch.max(output.data.cpu(), 1)
+            Acc += arg_max_out.long().eq(target.data.cpu().long()).sum()
+        
+        print("Loss: %0.3f | Acc: %0.2f"%(np.array(loss_cum).mean(), float(Acc*100)/len(x_train)))
+
+def test(x_test, y_test, model):
+    for epoch in range(epochs):  
+        model.eval()
+        loss_cum = []
+        Acc = 0
+        for batch_idx in tqdm.tqdm(range(0, len(x_test), batch_size), desc="[TEST] Epoch: {}".format(epoch)):
+            x_batch = x_test[batch_idx:batch_idx+batch_size]
+            y_batch = y_test[batch_idx:batch_idx+batch_size]
+            data = torch.from_numpy(x_batch).float().to(device).requires_grad_(False)
+            target = torch.from_numpy(y_batch).float().to(device).requires_grad_(False)
+    
+            output = model(data)
+            loss = model.Loss(output,target)   
+            loss_cum.append(loss.item())
+            _, arg_max_out = torch.max(output.data.cpu(), 1)
+            Acc += arg_max_out.long().eq(target.data.cpu().long()).sum()
+        
+        print("Loss Test: %0.3f | Acc Test: %0.2f"%(np.array(loss_cum).mean(), float(Acc*100)/len(x_test)))
+    
+if __name__=='__main__':
+    epochs=20
+    batch_size=1000
+    TEST=False
+    x_train, y_train, x_test, y_test = get_data()
+
+    model = Net()
+    model.to(device)
+    model.training_params()
+    print_network(model, 'Conv network + fc 2 layer non-linearity')    
+    train(x_train, y_train, model)
+    if TEST: test(x_test, y_test, model)
