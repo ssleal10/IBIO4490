050782358f35cd1cc510794f8e8d8afe042f45c4
ssleal10 <47038702+ssleal10@users.noreply.github.com>
2019-04-10 22:14:28 -0500

    Update convEmotions.py

--- 11-NN/convEmotions.py
+++ 11-NN/convEmotions.py
@@ -20,11 +20,11 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         #layer with 64 2d convolutional filter of size 3x3
-        self.conv1 = nn.Conv2d(1, 480, kernel_size=3) #Channels input: 1, c output: 48, filter of size 3
-        self.conv2 = nn.Conv2d(480, 240, kernel_size=3)
-        self.conv3 = nn.Conv2d(240, 120, kernel_size=3)
-        self.fc1 = nn.Linear(1920, 480)   
-        self.fc2 = nn.Linear(480, 10)  
+        self.conv1 = nn.Conv2d(1, 520, kernel_size=5) #Channels input: 1, c output: 48, filter of size 3
+        self.conv2 = nn.Conv2d(520, 390, kernel_size=5)
+        self.conv3 = nn.Conv2d(390, 260, kernel_size=5)
+        self.fc1 = nn.Linear(1040, 520)   
+        self.fc2 = nn.Linear(520, 10) 
     
     def forward(self, x, verbose=False):
         if verbose: "Output Layer by layer"
@@ -40,7 +40,7 @@ class Net(nn.Module):
         x = F.dropout(x, 0.50, training=self.training)
         if verbose: print(x.size())
         #ipdb.set_trace()
-        x = x.view(-1, 1920)
+        x = x.view(-1, 1040)
         if verbose: print(x.size())
         x = F.relu(self.fc1(x))
         if verbose: print(x.size())
