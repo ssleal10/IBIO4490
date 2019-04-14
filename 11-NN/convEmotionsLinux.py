d47f963ce9cc523d21c3764be7baf69660a2e56f
ssleal10 <47038702+ssleal10@users.noreply.github.com>
2019-04-14 11:53:40 -0500

    Update convEmotionsLinux.py

--- 11-NN/convEmotionsLinux.py
+++ 11-NN/convEmotionsLinux.py
@@ -245,7 +245,7 @@ def test(data_loader, model, epoch):
     file.close()         
 
 if __name__=='__main__':
-    epochs=40
+    epochs=100
     batch_size=50 
     TEST=False
     x_train, y_train, x_val, y_val = get_data()
